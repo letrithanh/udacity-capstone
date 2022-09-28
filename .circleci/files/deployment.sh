a58be2dfb34651a55eca7e2df84d148cdb779f82
Le Tri Thanh <letrithanh@outlook.com>
2022-09-28 08:12:40 +0000

    fix wrong ident

--- .circleci/files/deployment.sh
+++ .circleci/files/deployment.sh
@@ -8,18 +8,18 @@ metadata:
 spec:
     replicas: 1
     selector:
-    matchLabels:
-        app: uda-capstone
+        matchLabels:
+            app: uda-capstone
     template:
-    metadata:
-        labels:
-        app: uda-capstone
-    spec:
-        containers:
-        - name: uda-capstone
-        image: $1
-        ports:
-        - containerPort: 3000
+        metadata:
+            labels:
+                app: uda-capstone
+        spec:
+            containers:
+            - name: uda-capstone
+            image: $1
+            ports:
+            - containerPort: 3000
 
 ---
 
@@ -30,9 +30,9 @@ metadata:
 spec:
     type: NodePort
     ports:
-    - port: 3000
-    targetPort: 3000
-    nodePort: 30080
+        - port: 3000
+        targetPort: 3000
+        nodePort: 30080
     selector:
-    app: uda-capstone
+        app: uda-capstone
 EOF
